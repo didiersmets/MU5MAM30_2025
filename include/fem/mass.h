aa5f44f729f40b5d81aefcbb3d92bab42332484c
Erwan <duwer.erwan@gmail.com>
2025-12-16 21:53:03 +0100

    function creating the mass matrix

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -15,7 +15,20 @@
  *
  * Hiden for now.
  */
+
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 {
-	/* Your implementation goes here ! */
+	double area = norm(cross(AB, AC)) * 0.5;
+	double diag = area / 12.0;
+	double non_diag = area / 24.0;
+
+	M[0] = diag; // M_11
+	M[1] = non_diag; // M_12
+	M[2] = non_diag; // M_13
+	M[3] = non_diag; // M_21
+	M[4] = diag; // M_22
+	M[5] = non_diag; // M_23
+	M[6] = non_diag; // M_31	
+	M[7] = non_diag; // M_32
+	M[8] = diag; // M_33
 }
