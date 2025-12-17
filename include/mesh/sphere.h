ef1b2b2f1296cb86738162cb7ed1189c0f98e181
lelecaruso <emanuele.carusowork@outlook.it>
2025-12-17 10:10:33 +0100

    new folder from main branch

--- include/mesh/sphere.h
+++ include/mesh/sphere.h
@@ -6,6 +6,8 @@ This structure stores only the "skin" of the object.
  * Ideal for surface-only PDEs (Laplace-Beltrami).
  */
 
-// it follows the same strategy as the cube generation code, but when adding a new vertex, it
-// normalizes its position vector to project it onto the sphere surface.
+//  Idead taken from cube.h , here the cube is projected onto a sphere.
+// it follows the same strategy as the cube generation code,
+// but when adding a new vertex,
+// it normalizes its position vector to project it onto the sphere surface.
 int load_sphere(Mesh& m, size_t subdiv);
--- src/mesh/sphere.cpp
+++ src/mesh/sphere.cpp
@@ -1,1 +1,9 @@
 // same as cube.cpp but for sphere
+
+int load_sphere(Mesh& m, size_t subdiv)
+{
+  // Uncomment when it will be implemeneted
+  // load_overlapping_sphere<float>(m, subdiv);
+  remove_duplicate_vertices(m);
+  return 1;
+}
