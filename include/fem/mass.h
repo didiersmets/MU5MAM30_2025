3ac9a6356582a2c2eb116f9665fa9652ab4684df
Yakine <rachediyakine@gmail.com>
2025-12-17 14:50:01 +0100

    Clarify results and fix mass matrix implementation

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -13,15 +13,42 @@
  * Idea behind computation :
  * -------------------------
  *
- * Hiden for now.
+ * Fonctions de base P1 :
+ * ---------------------
+ * Les fonctions de base φ_i sont affines sur le triangle ABC :
+ *     Phi_i(x,y) = a_i x + b_i y + c_i.
+ * Elles sont définies par :
+ *     Phi_i(S_j) = δ_ij -> φ_A(A)=1 et φ_A(B)=φ_A(C)=0 (idem pour B et C).
+ *
+ * Matrice de masse locale :
+ * ------------------------
+ * La matrice de masse M est définie par :
+ *     M_ij = ∫_ABC φ_i φ_j dABC
+ * Explicitement :
+ *     M = [ ∫ Phi_A^2   ∫ Phi_A Phi_B   ∫ Phi_A Phi_C
+ *           ∫ Phi_B Phi_A ∫ Phi_B^2     ∫ Phi_B Phi_C
+ *           ∫ Phi_C Phi_A ∫ Phi_C Phi_B   ∫ Phi_C^2 ]
+ *
+ * Calcul pratique :
+ * -----------------
+ * Le triangle ABC (éventuellement plongé dans R^3) est obtenu par
+ * une application affine F du triangle de référence T̂ ⊂ R^2.
+ * Les fonctions de base physiques Phi_i sont les images des fonctions
+ * de base de référence Phi^_i par F
+ *
+ * Par changement de variables :
+ *     M_ij = ∫_T̂ φ̂_i φ̂_j |det(J_F)| dT̂,
+ * où J_F est la matrice jacobienne de F.
+ *
+ * Pour un élément P1, |det(J_F)| est constant et vaut 2|ABC|,
+ * ce qui conduit à la formule :
+ *
+ *     M = (|ABC| / 24) * [ 2  1  1
+ *                         1  2  1
+ *                         1  1  2 ]
  */
+
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M) {
-	double aire = 1/2 * norm(cross(AB, AC)); // norme du produit vectoriel
-	double coeff = aire / 12.0;
-	const int dim = 3;
-	for(int i = 0; i < dim; ++i){
-		for(int j = 0; j < dim; ++j){
-			i == j ? M[3 * i + j] = 2 * coeff : M[3 * i + j] = coeff;
-		}
-	}
+	M[0] = norm(cross(AB, AC)) / 12;
+	M[1] = M[0] / 2;
 }
