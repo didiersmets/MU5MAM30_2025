a8b8ca2aafe5d462a231846a67634a64ba36e622
Yakine <rachediyakine@gmail.com>
2025-12-17 14:16:06 +0100

    Mass implementation

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -15,7 +15,13 @@
  *
  * Hiden for now.
  */
-void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
-{
-	/* Your implementation goes here ! */
+void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M) {
+	double aire = 1/2 * norm(cross(AB, AC)); // norme du produit vectoriel
+	double coeff = aire / 12.0;
+	const int dim = 3;
+	for(int i = 0; i < dim; ++i){
+		for(int j = 0; j < dim; ++j){
+			i == j ? M[3 * i + j] = 2 * coeff : M[3 * i + j] = coeff;
+		}
+	}
 }
