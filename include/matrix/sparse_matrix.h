f5e04dd124e0decfd37fe0cebb1a1150225ff0e9
lelecaruso <emanuele.carusowork@outlook.it>
2025-12-18 10:19:49 +0100

    Importata cartella include/matrix  da main

--- include/matrix/sparse_matrix.h
+++ include/matrix/sparse_matrix.h
@@ -1,8 +1,8 @@
 #pragma once
+#include <stdint.h>
+
 #include "array.h"
 #include "matrix.h"
-
-#include <stdint.h>
 
 /******************************************************************************
  *
@@ -10,36 +10,25 @@
  *
  *****************************************************************************/
 
-struct CSRPattern
-{
-  bool   symmetric;
-  size_t rows;
-  size_t cols;
-  size_t nnz;
-  /* Non zero entries on line i (0 <= i < rows)
-   * are stored at indices row_start(i) <= k < row_start(i + 1).
-   * Corresponding column indices are read into col(k). */
-  TArray<uint32_t> row_start; /* Size = nrows + 1 */
-  TArray<uint32_t> col;       /* Size = nnz */
+struct CSRPattern {
+	bool symmetric;
+	size_t rows;
+	size_t cols;
+	size_t nnz;
+	/* Non zero entries on line i (0 <= i < rows)
+	 * are stored at indices row_start(i) <= k < row_start(i + 1).
+	 * Corresponding column indices are read into col(k). */
+	TArray<uint32_t> row_start; /* Size = nrows + 1 */
+	TArray<uint32_t> col; /* Size = nnz */
 };
 
-struct CSRMatrix : public Matrix
-{
-  bool           symmetric = false;
-  size_t         nnz;       /* Number of (non zero) entries */
-  uint32_t*      row_start; /* pointer to the corresponding data in pattern */
-  uint32_t*      col;       /* pointer to the corresponding data in pattern */
-  TArray<double> data;      /* Size = nnz  */
-  void           mvp(const double* __restrict x, double* __restrict y) const;
-  double         sum() const;
-  double&        operator()(uint32_t i, uint32_t j);
+struct CSRMatrix : public Matrix {
+	bool symmetric = false;
+	size_t nnz; /* Number of (non zero) entries */
+	uint32_t *row_start; /* pointer to the corresponding data in pattern */
+	uint32_t *col; /* pointer to the corresponding data in pattern */
+	TArray<double> data; /* Size = nnz  */
+	void mvp(const double *__restrict x, double *__restrict y) const;
+	double sum() const;
+	double &operator()(uint32_t i, uint32_t j);
 };
-
-/* Visualisation of non zeros */
-void spy(const CSRPattern& P, uint32_t width, const char* fname);
-
-/******************************************************************************
- * Here is possible to add more sparse matrix formats
- * SKyLine sparse matrix
- *
- *****************************************************************************/
