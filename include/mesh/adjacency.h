b903afb31d66d16e432354f3689fbdcbfb600ee4
houlette <houlette@student.ubc.ca>
2025-12-19 10:48:43 +0100

    add: VTAdjacency structure

--- include/mesh/adjacency.h
+++ include/mesh/adjacency.h
@@ -4,15 +4,15 @@
 #include "mesh.h"
 
 /*****************************************************************************
- * The mesh structure (mesh.h) that we use does not provide an immediate way 
- * to determine which triangles are incident to a given vertex. 
- * Since that information is helpful in some situations (we shall use it e.g. 
+ * The mesh structure (mesh.h) that we use does not provide an immediate way
+ * to determine which triangles are incident to a given vertex.
+ * Since that information is helpful in some situations (we shall use it e.g.
  * to determine the CSR sparsity pattern of the associated mass and stiffness
- * matrices), we provide a dedicated structure, called VTAdjacency, than can 
+ * matrices), we provide a dedicated structure, called VTAdjacency, than can
  * be initialized on the fly from a given existing mesh, and which encodes that
  * information.
  *
- * The incident triangles to vertex number "a" are not encoded by their 
+ * The incident triangles to vertex number "a" are not encoded by their
  * in the triangles array, but instead by listing the indices of the two
  * remaining vertices, in order : more precisely, if one such triangle is
  * "abc" then the VTri struct below will record the pair (b,c), because "a"
@@ -26,19 +26,21 @@
  * - offset : an array of size m.vertex_count(), this is just an accumulation
  *            of the degrees or earlier vertices, it is used to inform in which
  *            starting position (in the array vtri below) the adjacency info
- *            of a given vertex can be found. In simple math terms : 
+ *            of a given vertex can be found. In simple math terms :
  *            offset[0] = 0 and offset[k] = \sum_{i = 0}^{k-1} degree[i]
  * - vtri   : an array of size 3 * m.triangle_count() [indeed each triangle
  *            lists 3 vertices so the total sum of the degrees is 3 times as
- *            much as the number of vertices]. The VTri structs (as explained 
+ *            much as the number of vertices]. The VTri structs (as explained
  *            above) related to vertex k are found in the vtri array at
- *            positions j such that offset[k] <= j < offser[k] + degree[k].
+ *            positions j such that offset[k] <= j < offset[k] + degree[k].
  *
  *****************************************************************************/
 
 /* Vertex to Triangle adjacency table */
-struct VTAdjacency {
-	struct VTri {
+struct VTAdjacency
+{
+	struct VTri
+	{
 		uint32_t next;
 		uint32_t prev;
 	};
@@ -47,4 +49,9 @@ struct VTAdjacency {
 	TArray<VTri> vtri;
 	/* The constructor that you need to implement in src/adjacency.cpp */
 	VTAdjacency(const Mesh &m);
+
+	/* Auxiliary functions */
+	void set_degree(uint32_t *idx, size_t nt);
+	void set_offset(size_t nt);
+	void set_vtri(uint32_t *idx, size_t nt);
 };
--- src/mesh/adjacency.cpp
+++ src/mesh/adjacency.cpp
@@ -1,7 +1,88 @@
 #include "mesh.h"
 #include "adjacency.h"
+#include <cstdint>
 
 VTAdjacency::VTAdjacency(const Mesh &m)
 {
-	/* Your implementation goes here */
+	size_t nv = m.vertex_count();
+	size_t nt = m.triangle_count();
+
+	/* Reserve memory */
+	degree.resize(nv);
+	offset.resize(nv);
+	vtri.resize(3 * nt);
+
+	/* Build degree */
+	set_degree(m.indices.data, nt);
+
+	/* Build offset */
+	set_offset(nv);
+
+	/* Build vtri */
+	set_vtri(m.indices.data, nt);
+}
+
+/* Auxiliary functions */
+
+void VTAdjacency::set_degree(uint32_t *idx, size_t nt)
+{
+	for (size_t tri = 0; tri < nt; tri++)
+		for (size_t l = 0; l < 3; l++)
+			degree[idx[3 * tri + l]] += 1;
+}
+
+void VTAdjacency::set_offset(size_t nv)
+{
+	offset[0] = 0;
+	for (size_t k = 1; k < nv; k++)
+		offset[k] = offset[k - 1] + degree[k - 1];
+}
+
+void VTAdjacency::set_vtri(uint32_t *idx, size_t nt)
+{
+	VTri zero;
+	zero.next = UINT32_MAX;
+	zero.prev = UINT32_MAX;
+	vtri.set(zero);
+
+	VTri current_pair;
+	size_t k;
+	for (size_t tri = 0; tri < nt; tri++)
+	{
+		/* Triplet (a, b, c) */
+		k = offset[3 * tri];
+		while (k < offset[3 * tri] + degree[3 * tri] && vtri[k].next != UINT32_MAX)
+			k++;
+
+		if (k < offset[3 * tri] + degree[3 * tri])
+		{
+			current_pair.next = idx[3 * tri];
+			current_pair.prev = idx[3 * tri + 1];
+			vtri[k] = current_pair;
+		}
+
+		/* Triplet (b, c, a) */
+		k = offset[3 * tri + 1];
+		while (k < offset[3 * tri + 1] + degree[3 * tri + 1] && vtri[k].next != UINT32_MAX)
+			k++;
+
+		if (k < offset[3 * tri + 1] + degree[3 * tri + 1])
+		{
+			current_pair.next = idx[3 * tri + 2];
+			current_pair.prev = idx[3 * tri];
+			vtri[k] = current_pair;
+		}
+
+		/* Triplet (c, a, b) */
+		k = offset[3 * tri + 2];
+		while (k < offset[3 * tri + 2] + degree[3 * tri + 2] && vtri[k].next != UINT32_MAX)
+			k++;
+
+		if (k < offset[3 * tri + 2] + degree[3 * tri + 2])
+		{
+			current_pair.next = idx[3 * tri];
+			current_pair.prev = idx[3 * tri + 1];
+			vtri[k] = current_pair;
+		}
+	}
 }
