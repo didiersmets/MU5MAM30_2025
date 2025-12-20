846d212b3390e61ffde844b89304fbacfd1edff2
Si-m0n <simondefradas@gmail.com>
2025-12-20 16:44:35 +0100

    Fix return error in mass matrix

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -21,15 +21,6 @@ void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 	Vec3d cross_product = cross(AB, AC);
 	double triangle_area = 0.5 * norm(cross_product);
 
-	double diag_coef = triangle_area / 6.0;
-	double off_diag_coef = triangle_area / 12.0;
-	M[0] = diag_coef;
-	M[1] = off_diag_coef;
-	M[2] = off_diag_coef;
-	M[3] = off_diag_coef;
-	M[4] = diag_coef;
-	M[5] = off_diag_coef;
-	M[6] = off_diag_coef;
-	M[7] = off_diag_coef;
-	M[8] = diag_coef;
+	M[0]= triangle_area / 6.0;
+	M[1] = triangle_area / 12.0;
 }
