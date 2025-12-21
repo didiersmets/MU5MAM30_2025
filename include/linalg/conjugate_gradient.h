a5123139ec3e339f21ceb6f7aad5aa30576ef9f1
houlette <houlette@student.ubc.ca>
2025-12-21 15:25:41 +0100

    add: CG one iteration

--- include/linalg/conjugate_gradient.h
+++ include/linalg/conjugate_gradient.h
@@ -5,13 +5,13 @@
  * vector computation.
  * In the implementation below we let the user responsible
  * for allocation of memory for the corresponding vectors, including
- * the internal ones : x is the solution vector, b the rhd, r is the 
+ * the internal ones : x is the solution vector, b the rhs, r is the 
  * residual vector (r = b - A * x), p is the direction of search 
  * vector, and Ap to store the product A*p.
  */
 
 /* Make one iteration (i.e. update x, r, p and Ap, and return the updated
- * value for the (squared, hence the name r2) norm or the residual.
+ * value for the (squared, hence the name r2) norm of the residual.
  */
 double cg_iterate_once(const Matrix &A, double *__restrict x,
 		       double *__restrict r, double *__restrict p,
