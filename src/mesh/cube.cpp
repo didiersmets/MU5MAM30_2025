0763716453193bfe6b3f75145351606ee4354d3f
Gaia <gaia.corengia.02@gmail.com>
2025-12-28 18:04:05 +0100

    implemented and completed

--- src/mesh/cube.cpp
+++ src/mesh/cube.cpp
@@ -19,11 +19,11 @@ int load_cube(Mesh &m, size_t subdiv)
 		return (-1);
 	}
 
-	size_t n = subdiv + 1;
+	size_t n = subdiv + 1;/*n is the number of vertices per face*/
 
 	/* Reserve memory for vertices and indices */
-	m.positions.resize(6 * POW2(n));
-	m.indices.resize(36 * POW2(subdiv));
+	m.positions.resize(6 * POW2(n)); /*6 faces, each with n^2 vertices--> it duplicates por triplicates vertices which are respectively on edge or angle of cube*/
+	m.indices.resize(36 * POW2(subdiv));/*6faces*3vertex per triangle*2 triagle per square*n^2 square per faces*/
 
 	/* First build vertices as six unattached faces of n^2 vertices each */
 	/* See below for implementation */
@@ -42,10 +42,54 @@ int load_cube(Mesh &m, size_t subdiv)
 
 static void load_cube_vertices(Vec3 *pos, size_t subdiv)
 {
-	/* Your implementation goes here */
+	k=0;
+	for (size_t f=0;f<6;f++){
+		 for (size_t i=0;i<subdiv+1;i++){
+			for (size_t j=0;j<subdiv+1;j++){
+		
+				if (f==0){/*face -X*/
+					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={-1, -1 + 2* j/(double)subdiv, -1 + i*2/(double)subdiv};
+				}
+				else if (f==1){/*face -Y*/
+					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv,-1, -1 + i*2/(double)subdiv};
+				}
+				else if (f==2){/*face -Z*/
+					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv, -1 + i*2/(double)subdiv, -1};
+				}
+				else if (f==3){/*face +X*/
+					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ 1, -1 + j*2/(double)subdiv, -1 + 2* i/(double)subdiv};
+				}
+				else if (f==4){/*face +Y*/
+					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv, 1, -1 + i*2/(double)subdiv};
+				}
+				else if (f==5){/*face +Z*/
+					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv, -1 + i*2/(double)subdiv, 1};
+				}
+
+			}
+		}	
+	}
 }
 
 static void load_cube_indices(uint32_t *idx, size_t subdiv)
 {
 	/* Your implementation goes here */
+	k=0;
+	for (size_t f=0;f<6;f++){
+		 for (size_t i=0;i<subdiv;i++){
+			for (size_t j=0;j<subdiv;j++){
+		
+				/*ora mi trovo in un quadratino di una face */
+				/*triangolo 1*/
+				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j;
+				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j+1;
+				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*(i+1)+j+1;
+				/*triangolo 2*/
+				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j;
+				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*(1+i)+j;
+				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*(1+i)+j+1;
+				
+			}
+		}	
+	}	
 }
