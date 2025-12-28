fbf520ea00496514bf6061850e36741686c3a951
Gaia <gaia.corengia.02@gmail.com>
2025-12-28 17:59:32 +0100

    Mass matrix function

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -15,7 +15,13 @@
  *
  * Hiden for now.
  */
+
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 {
-	/* Your implementation goes here ! */
+	Area=0.5*norm(cross(AB,AC));
+	Jacobian=2*Area /* lo Jacobiano è (e i triangoli sono piccoli Area triangolo/area tr ref (0.5))*/
+	/* Your implementation goes here */
+	M[0]=Jacobian/12; /*(phi_i)^2 */
+	M[1]=Jacobian/24; /*phi_i*phi_j  */
+	
 }
