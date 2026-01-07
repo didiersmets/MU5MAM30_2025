3daa1e9e9a2cb40a49f86f828fa03c1baea6b75c
Mattia Del Grande <mattiadelgrande@Mattias-MacBook-Pro.local>
2026-01-07 10:28:12 +0100

    New changes and new implementations

--- src/linalg/conjugate_gradient.cpp
+++ src/linalg/conjugate_gradient.cpp
@@ -18,6 +18,28 @@ double cg_iterate_once(const Matrix &A, double *__restrict x,
 	 * implementations heavilly make use of specific hardware capabilities 
 	 * (instruction sets, parallelism, cache efficiency etc). 
 	 */
+	size_t N = A.rows;
+	assert(A.rows == A.cols);
+
+	/* alpha_k = r_k^Tr_k / (p_k^T A p_k) */
+	A.mvp(p, Ap);
+	double alpha = r2 / blas_dot(p, Ap, N);
+
+	/* x_{k+1} = x_k + \alpha_k p_k */
+	blas_axpy(alpha, p, x, N);
+	/* r_{k+1} = r_k - \alpha_k Ap_k*/
+	blas_axpy(-alpha, Ap, r, N);
+
+	/* r2_new = r_{k+1}^T r_{k+1} */
+	double r2_new = blas_dot(r, r, N);
+
+	/* beta_k = r_{k+1}^Tr_{k+1} / (r_k^T r_k) */
+	/* p_{k+1} = r_{k+1} + beta_{k+1} p_k */
+	double beta = r2_new / r2;
+	blas_axpby(1, r, beta, p, N);
+
+	return (r2_new);
+
 }
 
 size_t conjugate_gradient_solve(const Matrix &A, const double *__restrict b,
--- src/mesh/cube.cpp
+++ src/mesh/cube.cpp
@@ -43,9 +43,53 @@ int load_cube(Mesh &m, size_t subdiv)
 static void load_cube_vertices(Vec3 *pos, size_t subdiv)
 {
 	/* Your implementation goes here */
+	size_t n = subdiv + 1;
+
+	/* First build vertices as six unattached faces of n^2 vertices each */
+	size_t voff[6];
+	for (int f = 0; f < 6; ++f) {
+		voff[f] = f * POW2(n);
+	}
+	float *dir = (float *)safe_malloc(n * sizeof(float));
+	float *rev = (float *)safe_malloc(n * sizeof(float));
+	for (size_t i = 0; i < n; ++i) {
+		dir[i] = (2 * (float)i - subdiv) / subdiv;
+	}
+	for (size_t i = 0; i < n; ++i) {
+		rev[i] = dir[subdiv - i];
+	}
+	for (size_t y = 0; y < n; ++y) {
+		for (size_t x = 0; x < n; ++x) {
+			pos[voff[0]++] = { dir[x], -1, dir[y] }; /* Front  */
+			pos[voff[1]++] = { rev[x], +1, dir[y] }; /* Back   */
+			pos[voff[2]++] = { -1, rev[x], dir[y] }; /* Left   */
+			pos[voff[3]++] = { +1, dir[x], dir[y] }; /* Right  */
+			pos[voff[4]++] = { dir[x], rev[y], -1 }; /* Bottom */
+			pos[voff[5]++] = { dir[x], dir[y], +1 }; /* Top    */
+		}
+	}
+	free(dir);
+	free(rev);
 }
 
 static void load_cube_indices(uint32_t *idx, size_t subdiv)
 {
-	/* Your implementation goes here */
+	size_t n = subdiv + 1;
+	/* Build corresponding triangulation indices */
+	for (int f = 0; f < 6; f++) {
+		size_t offset = f * POW2(n);
+		for (size_t i = 0; i < subdiv; ++i) {
+			for (size_t j = 0; j < subdiv; ++j) {
+				uint32_t base = (uint32_t)(i * n + j + offset);
+				/* First tri */
+				*idx++ = base;
+				*idx++ = base + 1;
+				*idx++ = base + 1 + n;
+				/* Second tri */
+				*idx++ = base;
+				*idx++ = base + 1 + n;
+				*idx++ = base + n;
+			}
+		}
+	}
 }
