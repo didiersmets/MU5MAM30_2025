129a994e471c028bb099e46030f097f99a8ba4e4
Sarah Berkane <sarahkeita@Ordinateur-portable-de-Sarah.local>
2026-01-11 15:41:06 +0100

    Modifications du fichier CmakeLists pour compiler et creation du dossier Build

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -2,6 +2,7 @@
 
 #include "vec3.h"
 
+
 /* Given a triangle ABC, computes the (symmetric) 3x3 mass M s.t.
  *
  *   M_{ij} := \int_{ABC} \phi_i \phi_j
@@ -17,18 +18,22 @@
  */
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 {
-	double aire = 1/2 * norm(cross(AB, AC)); // cross correspond au produit vectoriel en 3D, les fonctions norm et cross sont définies dans vec3.h
-	double coeff = aire / double(12);
-	int n = 3; 
-	for (int i = 0; i<n; i++){
-		for (int j = 0; j<n; j++){
-			if (i == j){
-				M[3*i + j] = 2*coeff;
-				}
-			else{
-				M[3*i + j] = coeff;
-			}
-		}
-	}
+	// double aire = 1/2 * norm(cross(AB, AC)); // cross correspond au produit vectoriel en 3D, les fonctions norm et cross sont définies dans vec3.h
+	// double coeff = aire / double(12);
+	// int n = 3; 
+	// for (int i = 0; i<n; i++){
+	// 	for (int j = 0; j<n; j++){
+	// 		if (i == j){
+	// 			M[3*i + j] = 2*coeff;
+	// 			}
+	// 		else{
+	// 			M[3*i + j] = coeff;
+	// 		}
+	// 	}
+	// }
+	double detJacobienne = norm(cross(AB,AC));
+
+	M[0] = detJacobienne/12.0; /*Coefficients diagonaux*/
+	M[1] = detJacobienne/24.0; /*Autres coefficients*/
 	
 }
--- include/fem/stiffness.h
+++ include/fem/stiffness.h
@@ -23,5 +23,28 @@
  */
 void inline stiffness(const Vec3d &AB, const Vec3d &AC, double *__restrict S)
 {
-	/* Your implementation goes here */
+	double norme2_AC = norm2(AC);
+	double norme2_AB = norm2(AB);
+	double pdt_scalaire = dot(AC,AB);
+	double norme_BC = norme2_AC - 2 * pdt_scalaire + norme2_AB; 
+
+	double Aire = (0.5)*(norm(cross(AB,AC)));
+
+	/* La matrice est symetrique donc on calcule que 6 coefficients*/
+    
+    /* On ecrit BC = AB - AC */
+    /* int (gradient phiA.gradient phiA) */
+	S[0] =  norme_BC /(4*Aire);  
+    /* int (gradient phiA.gradient phiB) */
+	S[1] = (pdt_scalaire - norme2_AC)/(4*Aire); 
+    /* int (gradient phiA.gradient phiC) */
+	S[2] = (norme2_AB - pdt_scalaire )/(4*Aire); 
+    /* int (gradient phiB.gradient phiB) */
+	S[3] =  norme2_AC /(4*Aire) ; 
+    /* int (gradient phiB.gradient phiC) */
+	S[4] =	pdt_scalaire /(4*Aire); 
+    /* int (gradient phiC.gradient phiC) */
+	S[5] =  norme2_AB/(4*Aire);                  
+
 }
+
