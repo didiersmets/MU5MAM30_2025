b658dc3abd445daec4dba8f3afb36b9e8b1cf14e
“Francisca <francisca.caprile@etu.sorbonne-universite.fr>
2026-01-14 14:51:23 +0100

    conjugate gradient

--- include/linalg/conjugate_gradient.h
+++ include/linalg/conjugate_gradient.h
@@ -17,6 +17,8 @@ double cg_iterate_once(const Matrix &A, double *__restrict x,
 		       double *__restrict r, double *__restrict p,
 		       double *__restrict Ap, double r2);
 
+
+			   
 /* The master function, that call cg_interate_once as long as "needed",
  * the latter being guided by a user defined max iteration count, and 
  * tolerance to error. Inited = false correspond to the whole classical
--- src/linalg/conjugate_gradient.cpp
+++ src/linalg/conjugate_gradient.cpp
@@ -7,18 +7,36 @@
 #include "tiny_blas.h"
 
 double cg_iterate_once(const Matrix &A, double *__restrict x,
-		       double *__restrict r, double *__restrict p,
-		       double *__restrict Ap, double r2)
+                       double *__restrict r, double *__restrict p,
+                       double *__restrict Ap, double r2)
 {
-	/* Your implementation goes here
-	 * Check usage of tiny_blas (BLAS = Basic Linear Algebra Subroutines) 
-	 * in include/linalg/tiny_blas.h
-	 * BLAS routines are key in efficient linear algebra computations;
-	 * we do not really care about best performance here, top notch 
-	 * implementations heavilly make use of specific hardware capabilities 
-	 * (instruction sets, parallelism, cache efficiency etc). 
-	 */
+    const size_t N = A.rows;
+
+    // 1) Ap = A * p
+    A.mvp(p, Ap);
+
+    // 2) alpha = (r·r) / (p·Ap)
+    double pAp = blas_dot(p, Ap, N);
+    double alpha = r2 / pAp;
+
+    // 3) x = x + alpha * p
+    blas_axpy(alpha, p, x, N);
+
+    // 4) r = r - alpha * Ap
+    blas_axpy(-alpha, Ap, r, N);
+
+    // 5) r2_new = r·r
+    double r2_new = blas_dot(r, r, N);
+
+    // 6) beta = r2_new / r2
+    double beta = r2_new / r2;
+
+    // 7) p = r + beta * p
+    blas_axpby(1.0, r, beta, p, N);
+
+    return r2_new;
 }
+
 
 size_t conjugate_gradient_solve(const Matrix &A, const double *__restrict b,
 				double *__restrict x, double *__restrict r,
