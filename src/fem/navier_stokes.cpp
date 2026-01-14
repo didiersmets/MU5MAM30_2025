b6097232f8d37cfd1c857ca964a9723f6295c3e5
Erwan <duwer.erwan@gmail.com>
2026-01-14 13:43:17 +0100

    Début pour compute_transport

--- src/fem/navier_stokes.cpp
+++ src/fem/navier_stokes.cpp
@@ -45,6 +45,25 @@ void NavierStokesSolver::compute_transport(double *T)
 	memset(T, 0, N * sizeof(double));
 
 	/* Your implementation goes here */
+	for (size_t i = 0; i < m.triangle_count();++i){
+		//Get traingle vertex indices
+		uint32_t i0 = m.indices[3*i];
+		uint32_t i1 = m.indices[3*i+1];
+		uint32_t i2 = m.indices[3*i+2];
+		//Get triangle vertex positions
+		Vec3 p0 = m.positions[i0];
+		Vec3 p1 = m.positions[i1];
+		Vec3 p2 = m.positions[i2];
+		//Compute the area of the triangle
+		Vec3 e1 = p1 - p0;
+		Vec3 e2 = p2 - p0;
+		Vec3 cross = Vec3(e1.y * e2.z - e1.z * e2.y,
+				  e1.z * e2.x - e1.x * e2.z,
+				  e1.x * e2.y - e1.y * e2.x);
+		double area = 0.5 * sqrt(cross.x * cross.x + cross.y * cross.y + cross.z * cross.z);
+		//Compute gradients of basis functions
+		Vec3 grad_phi0 ;
+	}
 }
 
 size_t NavierStokesSolver::compute_stream_function()
