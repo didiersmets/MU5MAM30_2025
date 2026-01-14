50c7f8aa088ae27a9d9d2dd541765bfb8bbef3f5
Bader Ben Amira <bader.ben-amira1@etu.univ-lorraine.fr>
2026-01-14 14:09:16 +0100

    stiffness

--- include/fem/stiffness.h
+++ include/fem/stiffness.h
@@ -23,5 +23,21 @@
  */
 void inline stiffness(const Vec3d &AB, const Vec3d &AC, double *__restrict S)
 {
+    double ab2   = norm2(AB);
+    double ac2   = norm2(AC);
+    double ab_ac = dot(AB, AC);
 
+    double det = ab2 * ac2 - ab_ac * ab_ac;
+    double scale = 0.5 / sqrt(det);
+
+    ab2   *= scale;
+    ac2   *= scale;
+    ab_ac *= scale;
+
+    S[0] = ac2 - 2.0 * ab_ac + ab2;
+    S[1] = ac2;
+    S[2] = ab2;
+    S[3] = ab_ac - ac2;
+    S[4] = -ab_ac;
+    S[5] = ab_ac - ab2;
 }
