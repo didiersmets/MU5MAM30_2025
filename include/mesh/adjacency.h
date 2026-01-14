181b598471640161a3e3fb84c442a8e77daf76e2
Bader Ben Amira <bader.ben-amira1@etu.univ-lorraine.fr>
2026-01-14 14:46:49 +0100

    adjacency

--- include/mesh/adjacency.h
+++ include/mesh/adjacency.h
@@ -48,3 +48,37 @@ struct VTAdjacency {
 	/* The constructor that you need to implement in src/adjacency.cpp */
 	VTAdjacency(const Mesh &m);
 };
+
+VTAdjacency::VTAdjacency(const Mesh &m) {
+	size_t vtx_count = m.vertex_count();
+	size_t tri_count = m.triangle_count();
+	size_t idx_count = 3* tri_count;
+	degree.resize(vtx_count);
+	offset.resize(vtx_count);
+	vtri.resize(idx_count);
+
+	for (size_t i=0; i<vtx_count; i++) {
+		degree[i] = 0;
+		offset[i] = 0;
+	}
+
+	for (size_t i=0; i<idx_count; i++) {
+		degree[m.indices[i]]++
+	}
+
+	for (size_t v=0; v<vtx_count; v++) {
+		offset[v] = offset[v-1] + degree[v-1];
+	}
+
+	for (size_t i=0; i<tri_count; i++) {
+		int a = m.indices[3*t+0];
+		int b = m.indices[3*t+1];
+		int c = m.indices[3*t+2];
+		vtri[offset[a]++] = { b, c };
+		vtri[offset[b]++] = { c, a };
+		vtri[offset[c]++] = { a, b };
+
+	}
+
+
+}
