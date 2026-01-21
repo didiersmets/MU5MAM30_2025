057828941e1c61cd85a4e453877b72e9bb2705f8
Bader Ben Amira <bader.ben-amira1@etu.univ-lorraine.fr>
2026-01-21 13:55:48 +0100

    sphere

--- include/mesh/sphere.h
+++ include/mesh/sphere.h
@@ -1,3 +1,15 @@
 #include "mesh.h"
 
 int load_sphere(Mesh &m, size_t subdiv);
+{
+    if (int res = load_cube(m, subdiv))
+        return (res);
+
+    Vec3 *pos = m.positions.data;
+    size_t vtx_count = m.positions.size;
+    for (size_t i = 0; i < vtx_count; ++i) {
+        pos[i] = normalized(pos[i]);
+    }
+
+    return (0);
+}
