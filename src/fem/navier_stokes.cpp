6caf0b08f38dabc3eeb7e07628e61984c5093d3d
Jules Herrmann <jules.herrmann@inria.fr>
2026-01-28 16:54:17 +0100

    implemented T

--- src/fem/navier_stokes.cpp
+++ src/fem/navier_stokes.cpp
@@ -32,28 +32,55 @@ NavierStokesSolver::NavierStokesSolver(const Mesh &m)
 
 void NavierStokesSolver::set_zero_mean(double *V)
 {
-	/* Your implementation goes here */
+	M.mvp(V,Ap.data);
+	double s = blas_sum_in_place(Ap.data, N);
+	for (size_t i = 0; i < N; ++i) {
+		V[i] -= s / vol;
+	}
 }
 
-void NavierStokesSolver::compute_transport(double *T)
+void NavierStokesSolver::compute_transport(double *T,double dt)
 {
-	memset(T, 0, N * sizeof(double));
-
-	/* Your implementation goes here */
+	// computes T(omega,psi) and adds it to T
+	// memset(T, 0, N * sizeof(double));
+	for (size_t t=0;t<m.index_count();t+=3){
+		uint32_t Ai = m.indices[t];
+		uint32_t Bi = m.indices[t+1];
+		uint32_t Ci = m.indices[t+2];
+		uint32_t points[3] = {Ai,Bi,Ci};
+		Vec3 AB = B-A;
+		Vec3 AC = C-A;
+		double sum = omega[Ai] + omega[Bi] + omega[Ci];
+		for (uint32_t k =0;k<3;k++)
+			T[points[k]]+= dt * sum * (psi[points[(k-1)%3]] - psi[points[(k+1)%3]]);
+		}
 }
 
 size_t NavierStokesSolver::compute_stream_function()
 {
+	// solves for psi in : S psi = -M omega
 	size_t iter = 0;
+	M.mvp(omega,Momega)
 
-	/* Your implementation goes here */
-
+	double rel_error;
+	iter = conjugate_gradient_solve(
+			S,
+			Momega,
+			psi,
+			r,
+			p,
+			Ap
+			&rel_error,
+			10e-6,
+			500,
+			inited);
 	return iter;
 }
 
 void NavierStokesSolver::time_step(double dt, double nu)
 {
 	compute_stream_function();
+	compute_transport(Momega,dt)
 
 	/**********************************************************************
 	 * Solve the system :
