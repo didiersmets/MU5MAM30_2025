1f4866206ea287d7df7d45f48012fa31eca1515d
houlette <houlette@student.ubc.ca>
2026-02-14 19:56:17 +0100

    add: mass.h and stiffness.h so that the solutions files still run

---
+++ include/fem/mass.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "vec3.h"
+
+/* Given a triangle ABC, computes the (symmetric) 3x3 mass M s.t.
+ *
+ *   M_{ij} := \int_{ABC} \phi_i \phi_j
+ *
+ * where \phi_0 := \phi_A, \phi_1 := \phi_B, \phi_2 := \phi_C
+ * are the shape functions of the P1 Lagrange element associated
+ * to ABC.
+ */
+void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
+{
+	/* Computation of ||AB x AC|| */
+	double K = norm(cross(AB, AC));
+
+	if (K == 0)
+		throw std::runtime_error("The two vectors must be linearly independent.");
+	else
+	{
+		/* Computation of the coefficients */
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				M[3 * i + j] = (i == j) ? K / 12 : K / 24;
+	}
+}
---
+++ include/fem/stiffness.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "vec3.h"
+
+/* Given a triangle ABC, computes the (symmetric) 3x3 stiffness matrix S s.t.
+ *
+ *   S_{ij} := \int_{ABC} \nabla \phi_i \cdot \nabla \phi_j
+ *
+ * where \phi_0 := \phi_A, \phi_1 := \phi_B, \phi_2 := \phi_C
+ * are the shape functions of the P1 Lagrange element associated
+ * to ABC.
+ *
+ * Input : the vectors AB and AC.
+ * Output: the six coefficients S_{00} S_{11} S_{22} S_{01} S_{12} S_{20},
+ *         corresponding to the interactions A<->A, B<->B, C<->C, A<->B, B<->C,
+ *         C<->A
+ */
+void inline stiffness(const Vec3d &AB, const Vec3d &AC, double *__restrict S)
+{
+	/* Computation of ||AB x AC|| */
+	double K = norm(cross(AB, AC));
+	
+	if (K == 0)
+		throw std::runtime_error("The two vectors must be linearly independent.");
+	else
+	{
+		Vec3d BC = AC - AB;
+		S[0] = norm2(BC) / (2 * K); // A <-> A
+		S[1] = norm2(AC) / (2 * K); // B <-> B
+		S[2] = norm2(AB) / (2 * K); // C <-> C
+		S[3] = -dot(AC, BC) / (2 * K); // A <-> B
+		S[4] = -dot(AB, AC) / (2 * K); // B <-> C
+		S[5] = dot(AB, BC) / (2 * K); // C <-> A
+	}
+}
