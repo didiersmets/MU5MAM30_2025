223b1cf66bb556eb4c4c19842b1e13b909fc4ba4
Gaia <gaia.corengia.02@gmail.com>
2026-03-10 09:55:33 +0100

    new functions

--- src/mesh/cube.cpp
+++ src/mesh/cube.cpp
@@ -9,87 +9,81 @@
 #include "sys_utils.h"
 #include "vec3.h"
 
-static void load_cube_vertices(Vec3 *pos, size_t subdiv);
-static void load_cube_indices(uint32_t *idx, size_t subdiv);
 
-int load_cube(Mesh &m, size_t subdiv)
-{
-	/* Check subdiv is reasonable and return error if not */
-	if (subdiv <= 0 || subdiv > (1 << 14) /* 16K */) {
-		return (-1);
-	}
-
-	size_t n = subdiv + 1;/*n is the number of vertices per face*/
-
-	/* Reserve memory for vertices and indices */
-	m.positions.resize(6 * POW2(n)); /*6 faces, each with n^2 vertices--> it duplicates por triplicates vertices which are respectively on edge or angle of cube*/
-	m.indices.resize(36 * POW2(subdiv));/*6faces*3vertex per triangle*2 triagle per square*n^2 square per faces*/
-
-	/* First build vertices as six unattached faces of n^2 vertices each */
-	/* See below for implementation */
-	load_cube_vertices(m.positions.data, subdiv);
-
-	/* Build corresponding triangulation indices */
-	/* See below for implementation */
-	load_cube_indices(m.indices.data, subdiv);
-
-	/* Finally attach faces between themselves */
-	/* Implementation in src/duplicate_verts.cpp */
-	remove_duplicate_vertices(m);
-
-	return (0);
-}
 
 static void load_cube_vertices(Vec3 *pos, size_t subdiv)
 {
-	k=0;
-	for (size_t f=0;f<6;f++){
-		 for (size_t i=0;i<subdiv+1;i++){
-			for (size_t j=0;j<subdiv+1;j++){
-		
-				if (f==0){/*face -X*/
-					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={-1, -1 + 2* j/(double)subdiv, -1 + i*2/(double)subdiv};
-				}
-				else if (f==1){/*face -Y*/
-					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv,-1, -1 + i*2/(double)subdiv};
-				}
-				else if (f==2){/*face -Z*/
-					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv, -1 + i*2/(double)subdiv, -1};
-				}
-				else if (f==3){/*face +X*/
-					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ 1, -1 + j*2/(double)subdiv, -1 + 2* i/(double)subdiv};
-				}
-				else if (f==4){/*face +Y*/
-					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv, 1, -1 + i*2/(double)subdiv};
-				}
-				else if (f==5){/*face +Z*/
-					pos[(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j]={ -1 + 2* j/(double)subdiv, -1 + i*2/(double)subdiv, 1};
-				}
+	size_t n = subdiv + 1;
 
-			}
-		}	
+	/* First build vertices as six unattached faces of n^2 vertices each */
+	size_t voff[6];
+	for (int f = 0; f < 6; ++f) {
+		voff[f] = f * POW2(n);
 	}
+	float *dir = (float *)safe_malloc(n * sizeof(float));
+	float *rev = (float *)safe_malloc(n * sizeof(float));
+	for (size_t i = 0; i < n; ++i) {
+		dir[i] = (2 * (float)i - subdiv) / subdiv;
+	}
+	for (size_t i = 0; i < n; ++i) {
+		rev[i] = dir[subdiv - i];
+	}
+	for (size_t y = 0; y < n; ++y) {
+		for (size_t x = 0; x < n; ++x) {
+			pos[voff[0]++] = { dir[x], -1, dir[y] }; /* Front  */
+			pos[voff[1]++] = { rev[x], +1, dir[y] }; /* Back   */
+			pos[voff[2]++] = { -1, rev[x], dir[y] }; /* Left   */
+			pos[voff[3]++] = { +1, dir[x], dir[y] }; /* Right  */
+			pos[voff[4]++] = { dir[x], rev[y], -1 }; /* Bottom */
+			pos[voff[5]++] = { dir[x], dir[y], +1 }; /* Top    */
+		}
+	}
+	free(dir);
+	free(rev);
 }
 
 static void load_cube_indices(uint32_t *idx, size_t subdiv)
 {
-	/* Your implementation goes here */
-	k=0;
-	for (size_t f=0;f<6;f++){
-		 for (size_t i=0;i<subdiv;i++){
-			for (size_t j=0;j<subdiv;j++){
-		
-				/*ora mi trovo in un quadratino di una face */
-				/*triangolo 1*/
-				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j;
-				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j+1;
-				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*(i+1)+j+1;
-				/*triangolo 2*/
-				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*i+j;
-				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*(1+i)+j;
-				idx[k++]=(subdiv+1)*(subdiv+1)*f+(subdiv+1)*(1+i)+j+1;
-				
+	size_t n = subdiv + 1;
+	/* Build corresponding triangulation indices */
+	for (int f = 0; f < 6; f++) {
+		size_t offset = f * POW2(n);
+		for (size_t i = 0; i < subdiv; ++i) {
+			for (size_t j = 0; j < subdiv; ++j) {
+				uint32_t base = (uint32_t)(i * n + j + offset);
+				/* First tri */
+				*idx++ = base;
+				*idx++ = base + 1;
+				*idx++ = base + 1 + n;
+				/* Second tri */
+				*idx++ = base;
+				*idx++ = base + 1 + n;
+				*idx++ = base + n;
 			}
-		}	
-	}	
+		}
+	}
+}
+
+int load_cube(Mesh &m, size_t subdiv)
+{
+	if (subdiv <= 0 || subdiv > (1 << 14) /* 16K */) {
+		return (-1);
+	}
+
+	size_t n = subdiv + 1;
+
+	/* Reserve vertices and indices */
+	m.positions.resize(6 * POW2(n));
+	m.indices.resize(36 * POW2(subdiv));
+
+	/* First build vertices as six unattached faces of n^2 vertices each */
+	load_cube_vertices(m.positions.data, subdiv);
+
+	/* Build corresponding triangulation indices */
+	load_cube_indices(m.indices.data, subdiv);
+
+	/* Finally attach faces between themselves */
+	remove_duplicate_vertices(m);
+
+	return (0);
 }
