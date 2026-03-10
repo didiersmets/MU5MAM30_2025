d41e4322d21d0a88e0bc726e4b7b32786212c93d
Marius Kaletta <Marius.Kaletta@etu.sorbonne-universite.fr>
2026-03-10 16:16:01 +0100

    Modified in order to run the project on Windows

--- include/common/gl_utils.h
+++ include/common/gl_utils.h
@@ -16,6 +16,22 @@
 	#define GL_SILENCE_DEPRECATION 1
 	#include <OpenGL/gl3.h>
 	#include <OpenGL/glext.h>
+#elif defined _WIN32
+	#ifndef NOMINMAX
+    #define NOMINMAX
+    #endif
+
+	#include <windows.h>
+
+	#ifdef near
+    #undef near
+    #endif
+    #ifdef far
+    #undef far
+    #endif
+
+    #include <GL/glew.h>
+    #include <GL/gl.h>
 #else
 	#error "This platform doesn't seem supported yet."
 #endif
