64ed6eb96cc7292d2509b485ed5ef20faec65781
Gaia <gaia.corengia.02@gmail.com>
2026-03-10 09:56:17 +0100

    added dirichlet BCs

--- src/fem/poisson.cpp
+++ src/fem/poisson.cpp
@@ -13,7 +13,7 @@
 #include "boundary.h"
 
 PoissonSolver::PoissonSolver(const Mesh &m)
-    : m(m), N(m.vertex_count()), f(N), u(N, 0.0), r(N), p(N), Ap(N)
+    : m(m), N(m.vertex_count()), f(N), u(N, 0.0), r(N), p(N), Ap(N),is_bnd(N),g(N),b(N),ug(N)
 {
 #if USE_FEM_MATRIX
 	build_P1_mass_matrix(m, M);
@@ -27,21 +27,23 @@ PoissonSolver::PoissonSolver(const Mesh &m)
 	inited = false;
 	iterate = 0;
 	converged = false;
+	lifted = false;
+	
 	compute_boundary_vertices(m,is_bnd);
-	g.resize(N);
-	b.resize(N);
+	
+	
 	for (size_t i = 0; i < N; ++i) {
 
-    double x = veritces[i].x;
-    double y = vertices[i].y;
+    double x = m.positions[i].x;
+    double y = m.positions[i].y;
 
     // RHS
     b[i] =0;
 
-    // bordo non omogeneo esempio affine
-    if (is_boundary[i]) {
-		g[i] = 0.0;
-       // g[i] = 1.0 + x + 2.0*y;
+    // non_homogenenus boundary ( affine)
+    if (is_bnd[i]) {
+		//g[i] = 0.0;
+        g[i] = 1.0 + x + 2.0*y;
     } else {
         g[i] = 0.0;
     }
@@ -50,7 +52,12 @@ PoissonSolver::PoissonSolver(const Mesh &m)
 
 void PoissonSolver::clear_solution()
 {
-	build_P1_stiffness_matrix(m, P, A);
+	lifted = false;
+	#if USE_FEM_MATRIX
+		build_P1_stiffness_matrix(m, A);
+	#else
+		build_P1_stiffness_matrix(m, P, A);
+	#endif
 	for (size_t i = 0; i < N; i++) {
 		u[i] = 0;
 	}
@@ -70,95 +77,83 @@ void PoissonSolver::clear_solution()
 	}
 }*/ 
 
+
+
+
 void PoissonSolver::init_cg()
 {
-	double *F = f.data;
-	double *U = u.data;
-	double *R = r.data;
+	double *F = f.data;//f evaluated in nodes
+	double *U = u.data;//unknown
+	double *R = r.data;//residue
 	double *Pv = p.data;
-	double *AP = Ap.data;
+	double *AP = Ap.data;//A*p
+	lifted = false;
+	// 0) we build A, we will modify it imposing boundary condition 
+	#if !USE_FEM_MATRIX
+	build_P1_stiffness_matrix(m, P, A);
+	#endif
 	
-	/* Fix up F and U for zero mean */
-	/*set_zero_mean(F);
-	set_zero_mean(U);*/
+    
+    double *B = b.data;// right-hand side values at vertices
+    M.mvp(F, B);// B = M f
 
-	//1--I set buondary conditions
+	//all I need to move on A
+	uint32_t *rs = A.row_start;
+	uint32_t *cc = A.col;
+	double   *Ad = A.data.data;//It is A.data.data because A.data is a TArray<double> and I need a pointer to the data
+
+	// 1) I set Ug[i] = g[i] for boundary vertices, (rememebering U[i]=0 for internal vertices)
+	double *Ug = ug.data;   // new vector
+
 	for (size_t i = 0; i < N; ++i) {
-        if (is_bnd[i]) {
-            U[i] = g[i];
-        }
-    }
-	//2--I compute and b2
-	 if (b.size != N) {
-        b.resize(N);
-    }
-    double *B = b.data;
-    M.mvp(F, B);//B = Mf
 
-// Puntatori CSR per A
-    uint32_t *rs = A.row_start;//tells me where each row starts in the col and data arrays
-    uint32_t *cc = A.col;//tells me the column index of each nonzero entry in A
-    double   *Ad = A.data.data;//nonnzero values of A
+		if (is_bnd[i]) {
+			Ug[i] = g[i];
+		} else {
+			Ug[i] = 0.0;
+		}
+    	U[i]  = 0.0;   // incognita correttiva iniziale
+	}	
 
-    // --- 3) Correzione RHS: B_i -= sum_{j boundary} A_ij * g_j
-    // (si applica bene per tutti; per i di bordo poi sovrascriviamo comunque)
-    for (uint32_t i = 0; i < (uint32_t)N; ++i) {
-        double corr = 0.0;
-		//nel secondo for sta facendo il profotto tra A e ug (dove ug è zero se non sono di bordo e g[i] se sono di bordo) e lo sta sottraendo a B[i]
-        for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {//evaluate elements in row i
-            uint32_t j = cc[k];//column index of the current element
-            if (is_bnd[j]) {
-                corr += Ad[k] * g[j];
-            }
-        }
-        B[i] -= corr;
-    }
-	// --- 3b) Per mantenere A simmetrica per CG:
-	// per ogni riga interna i, azzera A(i,j) se j è di bordo.
+	// 2) B = B - A*Ug
+    A.mvp(Ug, R);
+    blas_axpy(-1.0, R, B, N);
+
+	// 3) I set columns  of boundary vertices to 0, if not diagonal. 
 	for (uint32_t i = 0; i < (uint32_t)N; ++i) {
-   	 if (is_bnd[i]) continue; // riga di bordo la sistemo dopo
+		if (is_bnd[i]) continue;//skip internal vertices
 
-    	for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {
-        uint32_t j = cc[k];
-        if (is_bnd[j]) {
-            Ad[k] = 0.0;
-        }
-    }
-}
-    // --- 4) Impone Dirichlet nella matrice e nel RHS:
-    // per ogni i di bordo: riga i = e_i^T, B_i = g_i
-    for (uint32_t i = 0; i < (uint32_t)N; ++i) {
-        if (!is_bnd[i]) continue;
+		
+		for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {
+			uint32_t j = cc[k];
+			if (is_bnd[j]) Ad[k] = 0.0;
+		}
+		
+	}
+	// 3) I set rows  of boundary vertices equal to identity. 
+	for (uint32_t i = 0; i < (uint32_t)N; ++i) {
+		if (!is_bnd[i]) continue;//skip external vertices rows
 
-        // azzera riga i
-        for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {
-            Ad[k] = 0.0;
-        }
+		for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {
+			Ad[k] = 0.0;
+		}
+		
+		for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {
+			
+			if (cc[k] == i) {Ad[k] = 1.0;
+			break;}
+		}
+		B[i] = 0;// sto imponenndo u0=0 sul bordo.
+	}
 
-        // metti diagonale A(i,i)=1 (assumiamo che (i,i) sia nel pattern, cosa vera per stiffness P1)
-        bool found_diag = false;
-        for (uint32_t k = rs[i]; k < rs[i + 1]; ++k) {
-            if (cc[k] == i) {
-                Ad[k] = 1.0;
-                found_diag = true;
-                break;
-            }
-        }
+// --- 5) initialize CG un A U = B
 
-        B[i] = g[i];// sta sovrascrivendo solo il valore di B[i] per i di bordo, mettendolo uguale a g[i]
-    }
-
-
-	// --- 5) Ora inizializza CG su A U = B
-
-    // b2 = ||B||^2 (per errore relativo)
+    // b2 = ||B||^2 (for relative error)
     b2 = blas_dot(B, B, N);
 
-    // R = B - A U
-    A.mvp(U, R);//R = AU;
-    blas_axpy(-1.0, R, B, N);   // B = B - AU (uso B come temporaneo)
-    blas_copy(B, R, N);         // R = B - AU
-
+    
+    blas_copy(B, R, N);   // perché U=0
+  
     // P = R
     blas_copy(R, Pv, N);
 
@@ -184,6 +179,16 @@ void PoissonSolver::do_iterate(size_t max_iter, double tol)
 		r2 = cg_iterate_once(A, U, R, Pv, AP, r2);
 		iterate++;
 		rel_error = sqrt(r2 / b2);
+		if (rel_error <= tol) {
+			converged = true;
+
+			if (!lifted) {
+				double *Ug = ug.data;
+				for (size_t i = 0; i < N; ++i)
+					U[i] += Ug[i];
+				lifted = true;
+			}
+		}
 	}
 
 	if (rel_error <= tol)
