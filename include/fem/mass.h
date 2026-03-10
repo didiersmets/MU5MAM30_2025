46d0c6fca5313bf29f8c872ef3185cfc0d4d9181
Gaia <gaia.corengia.02@gmail.com>
2026-03-10 09:48:35 +0100

    fixed error

--- include/fem/mass.h
+++ include/fem/mass.h
@@ -18,8 +18,8 @@
 
 void inline mass(const Vec3d &AB, const Vec3d &AC, double *__restrict M)
 {
-	Area=0.5*norm(cross(AB,AC));
-	Jacobian=2*Area /* lo Jacobiano è (e i triangoli sono piccoli Area triangolo/area tr ref (0.5))*/
+	double Area=0.5*norm(cross(AB,AC));
+	double Jacobian=2*Area;/* lo Jacobiano è (e i triangoli sono piccoli Area triangolo/area tr ref (0.5))*/
 	/* Your implementation goes here */
 	M[0]=Jacobian/12; /*(phi_i)^2 */
 	M[1]=Jacobian/24; /*phi_i*phi_j  */
